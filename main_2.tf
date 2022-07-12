a962a97d1b104c4645e4366052878d49d11d7264
sunnykid <sunnykid7@gmail.com>
2022-07-12 21:55:39 +0900

    Create main_2.tf
    aws에 최신버전의 ubuntu 설치

---
+++ main_2.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+    region = "ap-northeast-2"
+}
+
+resource "aws_instance" "web" {
+    ami = data.aws_ami.ubuntu.image_id
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "gooode-ubuntu"
+    }
+}
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
